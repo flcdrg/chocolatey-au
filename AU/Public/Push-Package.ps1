a702fe275ca4673f3f4fbee53e8333f469377f55
David Gardiner <david@gardiner.net.au>
2023-03-13 17:35:33 +1030

    More logging

--- AU/Public/Push-Package.ps1
+++ AU/Public/Push-Package.ps1
@@ -28,7 +28,8 @@ function Push-Package() {
     if (!$packages) { throw 'There is no nupkg file in the directory'}
     if ($api_key) {
         $packages | ForEach-Object {
-            Get-Item $_.FullName | Format-Table | Out-String | Write-Information
+            $_ | Format-Table | Out-String | Write-Information
+            Test-Path $_ | Write-Warning
             Write-Warning "choco push `"$($_.FullName)`" --api-key $api_key --source $push_url $force_push"
             choco push "$($_.FullName)" --api-key $api_key --source $push_url $force_push 
         }
